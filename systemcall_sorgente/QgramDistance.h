cb35c6d97b6eb1837208d5acbe2b3dbb813afac2
alberto <alberto@Tambu>
2019-06-07 01:41:55 +0200

    aggiungi tutti

---
+++ systemcall_sorgente/QgramDistance.h
@@ -0,0 +1,19 @@
+/*
+ * Q_gramDistance.h
+ *
+ *  Created on: 3 giu 2019
+ *      Author: tambu
+ */
+
+#ifndef QGRAMDISTANCE_H_
+#define QGRAMDISTANCE_H_
+
+long fattoriale(long);
+
+void tuttiQGramDistance(int, int[], int[], int);
+
+long dimQGramProfile(const int);
+
+void qGramProfile(int*, int, char[], const int, const int);
+
+#endif /* QGRAMDISTANCE_H_ */
